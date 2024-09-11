09e6d6e765f9a1a0f113d2eb6c744e006f96371e
roshanacharya <roshanacharya@pop-os>
2024-09-11 22:52:10 +0545

    ok

---
+++ BootMVCProj21-EmbeddedServers/src/main/java/com/rosan/BootMvcProj21EmbeddedServersApplication.java
@@ -0,0 +1,13 @@
+package com.rosan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj21EmbeddedServersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj21EmbeddedServersApplication.class, args);
+	}
+
+}
---
+++ BootMVCProj21-EmbeddedServers/src/test/java/com/rosan/BootMvcProj21EmbeddedServersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMvcProj21EmbeddedServersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
